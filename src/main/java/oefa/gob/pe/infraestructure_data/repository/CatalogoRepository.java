446a6966533a376b6f78978548185e26efde70bb
Kevin <109251642+Pariona-nov@users.noreply.github.com>
2022-10-03 09:32:53 -0500

    31022

---
+++ src/main/java/oefa/gob/pe/infraestructure_data/repository/CatalogoRepository.java
@@ -0,0 +1,34 @@
+package oefa.gob.pe.infraestructure_data.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.SelectProvider;
+
+import oefa.gob.pe.application_dto.catalogo.DTOCatalogo;
+import oefa.gob.pe.application_dto.catalogo.DTOCatalogoSolicitud;
+import oefa.gob.pe.infraestructure_data.mapper.CatalogoMapper;
+
+
+@Mapper
+public interface CatalogoRepository {
+    @SelectProvider(type = CatalogoMapper.class, method = "listar")
+    List<DTOCatalogo> listar(DTOCatalogoSolicitud solicitud);
+
+    @SelectProvider(type = CatalogoMapper.class, method = "listarPadre")
+    List<DTOCatalogo> listarPadre(DTOCatalogoSolicitud solicitud);
+
+    @SelectProvider(type = CatalogoMapper.class, method = "insertar")
+    void insertar(DTOCatalogoSolicitud solicitud);
+
+    @SelectProvider(type = CatalogoMapper.class, method = "modificar")
+    void modificar(DTOCatalogoSolicitud solicitud);
+
+    @SelectProvider(type = CatalogoMapper.class, method = "setEstado")
+    void setEstado(DTOCatalogoSolicitud solicitud);
+
+    @SelectProvider(type = CatalogoMapper.class, method = "validar")
+    List<DTOCatalogo> validar(DTOCatalogoSolicitud solicitud);
+
+
+}
